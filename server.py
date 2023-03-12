fcb1c0e6e2c12e066034b94e55b44faabba836d2
griffinnewbold <griffinnewbold@gmail.com>
2023-03-12 17:51:49 +0000

    added info for our postgre database

--- server.py
+++ server.py
@@ -30,8 +30,8 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #     DATABASEURI = "postgresql://zy2431:123123@34.73.36.248/project1"
 #
 # Modify these with your own credentials you received from TA!
-DATABASE_USERNAME = ""
-DATABASE_PASSWRD = ""
+DATABASE_USERNAME = "gcn2106"
+DATABASE_PASSWRD = "3276"
 DATABASE_HOST = "34.148.107.47" # change to 34.28.53.86 if you used database 2 for part 2
 DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/project1"
 
